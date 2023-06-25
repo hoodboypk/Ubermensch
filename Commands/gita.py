5fddc3cbbc2827a5f4eff7e102f4bf75bbfe4196
Pratik Kumar <93330691+hoodboypk@users.noreply.github.com>
2023-06-25 18:35:34 +0530

    Add files via upload

---
+++ Commands/gita.py
@@ -0,0 +1,15 @@
+import random
+
+def generate_gita():
+  gita = ("But certainly, there was not any time in the past when I did not exist nor you, nor these rulers of men. And surely it is not that we all shall cease to exist after this.", 
+          "Just as the man in this body passes through the various stages of boyhood, youth, and old age, like so, he passes into another body after death. The wise know it and are not deluded.", 
+          "The soul is neither born, nor does it ever die; nor is it that having come to exist, It will ever cease to be.The soul is birth less,eternal, immortal and ageless. It is not destroyed when the body is destroyed.", 
+          "O son of Kunti, the contact between the senses and the sense objects gives rise to fleeting perceptions of happiness and distress. These are non-permanent, and come and go like the winter and summer seasons. O descendent of Bharat, one must learn to tolerate them without being disturbed.", 
+          "You have a right to perform your prescribed duties, but you are not entitled to the fruits of your actions. Never consider yourself to be the cause of the results of your activities, nor be attached to inaction.", "Just as the ocean remains undisturbed by the incessant flow of waters from rivers merging into it, likewise the sage who is unmoved despite the flow of desirable objects all around him attains peace, and not the person who strives to satisfy desires.", 
+          "It is far better to perform one's natural prescribed duty, though tinged with faults, than to perform another's prescribed duty, though perfectly. In fact, it is preferable to die in the discharge of one's duty, than to follow the path of another, which is fraught with danger.", 
+          "Thus knowing oneself (soul) to be superior to the material senses, mind, and intellect, O mighty-armed Arjuna, subdue the lower self (senses, mind, and intellect) by the higher self (strength of the soul), and kill this formidable enemy called lust.",
+          "There are three gates leading to the hell of self-destruction for the soul—lust, anger, and greed. Therefore, all should abandon these three.",
+          "Anyone who is steady in his determination for the advanced stage of spiritual realization and can equally tolerate the onslaughts of distress and happiness is certainly a person eligible for liberation.",
+          "No one who does good work will ever come to a bad end, either here or in the world to come.",
+          "He who has let go of hatred who treats all beings with kindness and compassion, who is always serene, unmoved by pain or pleasure, free of the “I” and “mine,” self-controlled, firm and patient, his whole mind focused on me —that is the man I love best.")
+  return random.choice(gita)
