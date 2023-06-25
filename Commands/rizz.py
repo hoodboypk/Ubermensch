0fa56171e6565d628a4b19779ba1a79512f78e50
Pratik Kumar <93330691+hoodboypk@users.noreply.github.com>
2023-06-25 18:36:52 +0530

    Add files via upload

---
+++ Commands/rizz.py
@@ -0,0 +1,11 @@
+import random
+
+def generate_rizz():
+  rizz = ("If tomorrow brings a new hope, I hope it brings you.",
+         "If x+u=10, then I assume x to be 9, because you're the one.",
+         "For everyday I miss you, for every hour I need you, for every minute I feel you, for every second I want you, Forever I love you.",
+         "I have late night conversations with the moon. He tells me about the Sun, I tell him about you.",
+         "I am the book that you never cared to read, but you're the verse I keep highlighting.")
+
+  
+  return random.choice(rizz)
